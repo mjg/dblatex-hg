87971bd51ecae36c24ded1b2194cb59c3863882e
marsgui <none@none>
2009-06-04 00:02:40 +0200

    Cleanup (remove Hg hook code) + dir addition more robust.

--- tools/cvstools/cvsext.py
+++ tools/cvstools/cvsext.py
@@ -18,7 +18,8 @@ class CVSSession:
             self._log = open(batch, "w")
 
     def _clearfiles(self):
-        self.adds = None or []
+        self.add_files = None or []
+        self.add_dirs = None or []
         self.dels = None or []
         self.chgs = None or []
         self.cmds = None or []
@@ -82,9 +83,9 @@ class CVSSession:
         for c in cmds:
             e = c.split("#")[0].strip()
             if e:
-                rc = self.execute(e)
+                rc = self.execute(c)
                 if rc != 0:
-                    print "'%s' failed" % e
+                    print "'%s' failed" % c
                     break
             i = i+1
             done.append("# %s" % c)
@@ -93,9 +94,12 @@ class CVSSession:
     def _getcommands(self):
         cmds = []
         commits = []
-        if self.adds:
-            cmds.append("cvs add %s" % " ".join(self.adds))
-            commits += self.adds
+        if self.add_dirs:
+            for d in self.add_dirs:
+                cmds.append("cvs add %s" % (d))
+        if self.add_files:
+            cmds.append("cvs add %s" % " ".join(self.add_files))
+            commits += self.add_files
         if self.dels:
             cmds.append("cvs rm -f %s" % " ".join(self.dels))
             commits += self.dels
@@ -105,6 +109,7 @@ class CVSSession:
         if commits:
             # FIXME: characters with accents    
             desc = self.desc.replace("'", r"'\''")
+            desc = desc.replace("#", r"'\#'")
             if "\n" in self.desc:
                 # multiline commit logs needs to use a temporary file
                 opt = "-F"
@@ -134,103 +139,44 @@ class CVSSession:
             if rc != 0:
                 raise OSError("exec failed")
 
+    def from_cset(self, cset):
+        # Get the changeset tag
+        tag = ""
+        tags = cset.getElementsByTagName("tag")
+        if tags: tag = tags[0].getAttribute("name")
 
-def cvscommit(cvs, cset):
-    # Get the changeset tag
-    tag = ""
-    tags = cset.getElementsByTagName("tag")
-    if tags: tag = tags[0].getAttribute("name")
+        # New CVS command set
+        self.rev = cset.getAttribute("rev")
+        self.cset = cset.getAttribute("node")
+        desc = cset.getElementsByTagName("description")[0]
+        desc = desc.childNodes[0].nodeValue
+        self.newset(desc, tag=tag)
+        self.log("# rev=%s\n" % self.rev)
 
-    # New CVS command set
-    rev = cset.getAttribute("rev")
-    cvs.cset = cset.getAttribute("node")
-    desc = cset.getElementsByTagName("description")[0]
-    desc = desc.childNodes[0].nodeValue
-    cvs.newset(desc, tag=tag)
-    cvs.log("# rev=%s\n" % rev)
+        # Grab the files to add/delete/commit
+        files = cset.getElementsByTagName("file")
+        for f in files:
+            path = f.getAttribute("path")
+            mode = f.getAttribute("mode")
 
-    # Grab the files to add/delete/commit
-    files = cset.getElementsByTagName("file")
-    for f in files:
-        path = f.getAttribute("path")
-        mode = f.getAttribute("mode")
+            # Ignore Tag machinery changesets
+            if path == ".hgtags":
+                continue
 
-        # Ignore Tag machinery changesets
-        if path == ".hgtags":
-            continue
+            if mode == "add":
+                dirs = path.split(os.path.sep)[:-1]
+                for i in range(0, len(dirs)):
+                    subdir = os.path.join(*dirs[:i+1])
+                    cvsd = os.path.join(subdir, "CVS")
+                    if not(os.path.isdir(cvsd)):
+                        if not(subdir in self.add_dirs):
+                            self.add_dirs.append(subdir)
+                self.add_files.append(path)
+            elif mode == "del":
+                self.dels.append(path)
+            else:
+                self.chgs.append(path)
 
-        if mode == "add":
-            dirs = path.split(os.path.sep)[:-1]
-            for i in range(0, len(dirs)):
-                subdir = os.path.join(*dirs[:i+1])
-                cvsd = os.path.join(subdir, "CVS")
-                if not(os.path.isdir(cvsd)):
-                    if not(subdir in cvs.adds):
-                        cvs.adds.append(subdir)
-            cvs.adds.append(path)
-        elif mode == "del":
-            cvs.dels.append(path)
-        else:
-            cvs.chgs.append(path)
-
-    # Refresh the batch file
-    # cvs.flush()
-
-
-def do_preupdate(ui, repo, parent1="", parent2=""):
-    global preupdate_node
-
-    # Remember the current working parent in the case of CVS failure
-    p = Popen("hg parents --template '{node}'", shell=True, stdout=PIPE)
-    preupdate_node = p.communicate()[0]
-
-
-def do_update(ui, repo, parent1="", parent2="", error=0):
-    global preupdate_node
-
-    # Now call CVS for real
-    cvs = CVSSession(batch="cvslog.txt")
-    rc = cvs.runbatch("cvslog.txt", replace=True)
-
-    # On trouble come back to known situation
-    if rc != 0:
-        os.system("hg update -r %s" % preupdate_node)
-
-
-def cvshook(ui, repo, hooktype="", **kwargs):
-
-    print kwargs
-    # return True
-    print "***", hooktype
-    if hooktype == "preupdate":
-        return do_preupdate(ui, repo, **kwargs)
-
-    if hooktype == "update":
-        return do_update(ui, repo, **kwargs)
-
-    if hooktype == "pretxnchangegroup":
-        cmd = "hg log -r tip:%s --debug --style %s" % (kwargs["node"], style)
-        print (cmd)
-        p = Popen(cmd, shell=True, stdout=PIPE)
-        data = p.communicate()[0]
-        print (data)
-        #return True
-
-        cvs = CVSSession(batch="cvslog.txt")
-
-        dom3 = parseString("<changegroup>" + data + "</changegroup>")
-
-        csets = dom3.getElementsByTagName("changeset")
-        rc = False
-        for cset in csets:
-            cvscommit(cvs, cset)
-
-        dom3.unlink()
-        return rc
-
-    if hooktype == "changegroup":
-        pass
-
-    if hooktype == "incoming":
-        pass
+        # Refresh the batch file
+        # self.flush()
 
