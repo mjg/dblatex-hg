e1c3c22a1d724adf424a52f86df0561564a75e96
marsgui <none@none>
2012-06-03 01:32:24 +0200

    Provision for future use.

--- lib/dbtexmf/dblatex/xetex/fcfallback.py
+++ lib/dbtexmf/dblatex/xetex/fcfallback.py
@@ -71,27 +71,34 @@ class FcFallbackFontSpec(DefaultFontSpec):
                 return fontspec
 
         # Find the first fcfont that has this char in its charset
-        fcfont = self.fcmanager.get_font_handling(char)
+        fcfonts = {}
+        for font_type in ("serif", "sans-serif", "monospace"):
+            fcfonts[font_type] = self.fcmanager.get_font_handling(char,
+                                                         family_type=font_type)
+        # FIXME: attribuer les autres fonts si font nexiste pas dans le type
         if not(fcfont):
             self._loghas2(self.id + "[?fc]", char)
             return self
 
         # Extend the fontspec group
-        fontspec = self.spawn_fontspec_from_fcfont(fcfont, char)
+        fontspec = self.spawn_fontspec_from_fcfonts(fcfonts, char)
         self._loghas2(fontspec.id + "[A fc]", char)
         return fontspec
 
-    def spawn_fontspec_from_fcfont(self, fcfont, char):
+    def spawn_fontspec_from_fcfonts(self, fcfonts, char):
         self.log.info("New fontspec '%s' matching U%X from fontconfig"\
-              % (fcfont.family, ord(char)))
+              % (fcfonts["serif"].family, ord(char)))
         # Create a new font
         fontspec = FontSpec()
         fontspec.id = fcfont.family
-        fontspec.transitions["enter"]["main"] = fcfont.family
+        fontspec.transitions["enter"]["main"] = fcfonts["serif"].family
+        fontspec.transitions["enter"]["sans"] = fcfonts["sans-serif"].family
+        fontspec.transitions["enter"]["mono"] = fcfonts["monospace"].family
         fontspec.add_char(char)
         fontspec.add_ignored(self._ignored)
         # Register the font and its related fontconfig object
-        self.fccache[fcfont.name] = fcfont
+        for fcfont in fcfonts.values():
+            self.fccache[fcfont.name] = fcfont
         self.add_subfont(fontspec)
         return fontspec
 
